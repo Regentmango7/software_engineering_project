8bfc975a6455c88200577165373105793cc30321
CoderManing <plschussler@gmail.com>
2022-10-13 17:54:28 -0400

    created basic background

--- miner_game.py
+++ miner_game.py
@@ -7,13 +7,23 @@ pygame.init()
 def save():
     print("Game is closed")
 
+screen = pygame.display.set_mode([640, 400])
+pygame.display.set_caption("Click Miners")
+background = Colors.black
+framerate = 60
+font = pygame.font.Font("freesansbold.ttf", 16)
+timer = pygame.time.Clock()
+
 # Main body of code
 running = True
 while running:
+    timer.tick(framerate)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             save()
             running = False
+    screen.fill(background)
 
+    pygame.display.flip()
 
 pygame.quit()
