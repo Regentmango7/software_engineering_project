68a9c71e40044068133c44b785b8997f8a2c2b04
CoderManing <plschussler@gmail.com>
2022-10-13 17:25:13 -0400

    Added color library

--- miner_game.py
+++ miner_game.py
@@ -5,6 +5,13 @@ import Colors
 pygame.init()
 
 # color library
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+black = (0, 0, 0)
+white = (255, 255, 255)
+purple = (127, 0 , 255)
+orange = (255, 165, 0)
 
 
 def save():
