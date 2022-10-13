552369596017d761532868df6171b381437ac69a
CoderManing <plschussler@gmail.com>
2022-10-13 18:34:44 -0400

    enabled clicking, and ore generation/storage

--- miner_game.py
+++ miner_game.py
@@ -15,8 +15,18 @@ framerate = 60
 font = pygame.font.Font("freesansbold.ttf", 16)
 timer = pygame.time.Clock()
 
-def drawCircle():
-    pygame.draw.circle(screen, Colors.black, (320, 300), 60, 60)
+#game variables
+oreAmount = 0
+orePerClick = 1
+coinAmount = 0
+
+def drawMine():
+    mineArea = pygame.draw.circle(screen, Colors.black, (320, 300), 60, 60) #The click circle to generate ores
+    clickValue = font.render(str(orePerClick), True, Colors.white)
+    totOre = font.render(str(oreAmount), True, Colors.black)
+    screen.blit(clickValue, (320, 300))
+    screen.blit(totOre, (10, 10))
+    return mineArea
 
 # Main body of code
 running = True
@@ -26,8 +36,11 @@ while running:
         if event.type == pygame.QUIT:
             save()
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if mineArea.collidepoint(event.pos):
+                oreAmount += 1
     screen.fill(background)
-    drawCircle()
+    mineArea = drawMine()
     pygame.display.flip()
 
 pygame.quit()
