566f4917faeac282e3471df65e029daa55f7fd07
Regentmango7 <regentmango7@gmail.com>
2022-10-13 17:14:58 -0400

    Added miner_game.py, main file for our project.

---
+++ miner_game.py
@@ -0,0 +1,21 @@
+# CLICKER GAME
+
+import pygame
+pygame.init()
+
+# color library
+
+
+def save():
+    print("Game is closed")
+
+# Main body of code
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            save()
+            running = False
+
+
+pygame.quit()
